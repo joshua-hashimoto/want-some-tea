bd184377c7de2c3f9a1c1919ba21b53e8abb6c89
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-05-15 00:20:12 +0900

    create: manager and theme to change Storybook title

---
+++ frontend/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  //   brandTitle: "Want Some Tea",
+  brandImage: "https://cdn-icons-png.freepik.com/512/4300/4300605.png",
+  brandUrl: "https://qiita.com",
+  brandTarget: "_self",
+});
