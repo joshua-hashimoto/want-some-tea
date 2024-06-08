8392414047a652ed57adedc7d450c899e48fa010
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-08 15:21:15 +0900

    create: RoomCreateモデルと関連するモデルを作成

---
+++ frontend/src/models/room.ts
@@ -0,0 +1,31 @@
+import dayjs from "dayjs";
+
+export type PurchaseItem = {
+  id: string;
+  name: string;
+  amount: number;
+};
+
+export type RoomDetailResponse = {
+  id: string;
+  title: string;
+  description?: string;
+  items: PurchaseItem[];
+  closingAt: string;
+};
+
+export type RoomDetail = {
+  id: string;
+  title: string;
+  description?: string;
+  items: PurchaseItem[];
+  closingAt: dayjs.Dayjs;
+};
+
+export type RoomCreateForm = {
+  name: string;
+};
+
+export type RoomCreateResponse = {
+  roomId: string;
+};
