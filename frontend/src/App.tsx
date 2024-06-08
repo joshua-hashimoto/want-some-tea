2dc5c5d55861a6b49a1008ca035da751d78a428e
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-08 15:29:05 +0900

    fix: add QueryClientProvider and ErrorBoundary to application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { RouterProvider } from "react-router-dom";
 
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from "@tanstack/react-query";
 import dayjs from "dayjs";
 
 import "dayjs/locale/ja";
@@ -11,9 +17,21 @@ dayjs.locale("ja");
 
 type Props = Record<string, unknown>;
 
-const App: React.FC<Props> = () => (
-  <ErrorBoundary fallback={<UnexpectedErrorPage />}>
-    <RouterProvider router={router} />
-  </ErrorBoundary>
-);
+const App: React.FC<Props> = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} fallback={<UnexpectedErrorPage />}>
+            <Suspense fallback={null}>
+              <RouterProvider router={router} />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </QueryClientProvider>
+  );
+};
 export default App;
