48398e58c882972b29d7ea5499cf7707fd23a319
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-22 12:53:45 +0900

    fix: apply custom form to RoomCreatePage

--- frontend/src/pages/RoomCreatePage/RoomCreatePage.tsx
+++ frontend/src/pages/RoomCreatePage/RoomCreatePage.tsx
@@ -1,7 +1,8 @@
-import { Button, Card, Col, Form, Input, Row } from "antd";
+import { Button, Card, Col, Input, Row } from "antd";
 
+import Form from "~/components/Form";
 import Page from "~/components/Page";
-import { useCreateRoom } from "~/hooks/useRoomsApi";
+import { useCreateRoom } from "~/hooks";
 import { RoomCreateForm } from "~/models";
 
 type RoomCreatePageProps = Record<string, unknown>;
@@ -21,33 +22,37 @@ const RoomCreatePage: React.FC<RoomCreatePageProps> = () => {
           <Row>
             <Col span={24}>
               <Form form={form} onFinish={onFinish}>
-                <Row>
-                  <Col span={24}>
-                    <Form.Item name="name">
-                      <Input />
-                    </Form.Item>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col span={24}>
-                    <Form.Item noStyle>
-                      <Row justify="end">
-                        <Col>
-                          <Form.Item shouldUpdate>
-                            {({ getFieldValue }) => {
-                              const roomName = getFieldValue("name");
-                              return (
-                                <Button htmlType="submit" disabled={!roomName}>
-                                  作成する
-                                </Button>
-                              );
-                            }}
-                          </Form.Item>
-                        </Col>
-                      </Row>
-                    </Form.Item>
-                  </Col>
-                </Row>
+                <Col span={24}>
+                  <Row>
+                    <Col span={24}>
+                      <Form.Item name="name">
+                        <Input />
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={24}>
+                      <Form.Item noStyle>
+                        <Row justify="end">
+                          <Col>
+                            <Form.Item shouldUpdate>
+                              {({ getFieldValue }) => {
+                                const roomName = getFieldValue("name");
+                                return (
+                                  <Button
+                                    htmlType="submit"
+                                    disabled={!roomName}>
+                                    作成する
+                                  </Button>
+                                );
+                              }}
+                            </Form.Item>
+                          </Col>
+                        </Row>
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Col>
               </Form>
             </Col>
           </Row>
