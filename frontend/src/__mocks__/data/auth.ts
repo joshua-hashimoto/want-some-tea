01ae59cd3876f420c1176512f8c431075cb80031
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-22 13:01:01 +0900

    fix: add mock data for login

--- frontend/src/__mocks__/data/auth.ts
+++ frontend/src/__mocks__/data/auth.ts
@@ -1,19 +1,7 @@
-import jwt from "jsonwebtoken";
-import { v4 as uuidv4 } from "uuid";
-
 import { SignInResponse } from "~/models/auth";
 
-const generateToken = (payload: Record<string, unknown>) => {
-  const secretKey = "yourSecretKey"; // Replace with your own secret key
-  const options = {
-    expiresIn: "1h", // Token expiration time
-  };
-
-  return jwt.sign(payload, secretKey, options);
-};
+export const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJ1c2VySWQiOiI3MTlkOGE5OC05NWM5LTRkOGQtYWI5Zi01ODMyN2ExNjlkMzMifQ.inlT0SSprYVYMIoCCvjftk-qjf1YEMbCsLMPjHB_gHI"
 
 export const signInResponse: SignInResponse = {
-  accessToken: generateToken({
-    userId: uuidv4(),
-  }),
+  accessToken,
 };
