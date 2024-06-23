facf749c7244b902dc5fb6a910f95c72a32510f3
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-23 15:07:39 +0900

    fix: use recoil value in ProtectedRoute

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,12 +1,15 @@
 import { Navigate, Outlet, OutletProps } from "react-router-dom";
 
+import { useRecoilValue } from "recoil";
+
 import { Routes } from "~/routers";
+import { isLoggedInAtom } from "~/store/auth";
 
 type Props = OutletProps;
 
 const ProtectedRoute: React.FC<Props> = ({ ...props }) => {
   // TODO: ログインしてないユーザーはエラー画面に遷移する
-  const isLoggedIn = true;
+  const isLoggedIn = useRecoilValue(isLoggedInAtom);
 
   if (!isLoggedIn) {
     return <Navigate to={Routes.error.unauthorized} />;
