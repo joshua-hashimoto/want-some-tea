a893850956251dbbe03ddc778d8d343e435f7e29
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-23 16:57:45 +0900

    fix: custom card component implementation was in a loop

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import { Card as AntdCard, CardProps as AntdCardProps } from "antd";
 type CardProps = AntdCardProps;
 
 const _Card: React.FC<CardProps> = ({ ...props }) => (
-  <Card css={{ width: "100%", maxWidth: 800, height: "auto" }} {...props} />
+  <AntdCard css={{ width: "100%", maxWidth: 800, height: "auto" }} {...props} />
 );
 
 const Card = _Card as React.FC<CardProps> & {
