c9149fdeaa1cc16486b31317fe76677f7a856702
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-06-23 20:45:10 +0900

    fix: add sign-in and sign-up route in Routes constant

--- frontend/src/routers/routes.ts
+++ frontend/src/routers/routes.ts
@@ -1,5 +1,9 @@
 export const Routes = {
   init: "",
+  auth: {
+    singIn: "/auth/sign-in",
+    signUp: "/auth/sign-up",
+  },
   rooms: {
     entry: "/rooms/entry",
     create: "/rooms/create",
