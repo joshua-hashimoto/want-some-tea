13c0725cbbbd2cdd6751283ae3c465d505a7d151
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-09-07 12:16:40 +0900

    fix: ユーザー情報取得メソッドが実際のDBに接続されるように修正

--- backend/app/users/services.py
+++ backend/app/users/services.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime, timedelta, timezone
-from typing import Annotated, Literal, Optional, Union
+from typing import Annotated, Optional, Union
 
 from fastapi import Depends, HTTPException, Request, status
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
@@ -10,9 +10,10 @@ from jose import jwt
 from jose.exceptions import JWTError
 
 from app.configs.settings import get_settings
-from app.users.schemas import User, UserIn_Pydantic
 
-logger = logging.getLogger("uvicorn.backend")
+from .schemas import User
+
+logger = logging.getLogger("uvicorn.api")
 
 
 settings = get_settings()
@@ -20,17 +21,6 @@ settings = get_settings()
 SECRET_KEY = settings.secret_key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
 
 
 class OAuth2PasswordBearerWithCookie(OAuth2):
@@ -68,23 +58,28 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
 ここでは、tokenUrlに設定されているエンドポイント/tokenにはprefixがされている
 ため、ここでの設定もそれに合わせてエンドポイントを設定する
 """
-oauth2_scheme_cookie = OAuth2PasswordBearerWithCookie(tokenUrl="auth/login")
+oauth2_scheme_cookie = OAuth2PasswordBearerWithCookie(tokenUrl="auth/sign-in")
 
 
-def get_user(db, username: str) -> User | None:
-    if username in db:
-        user_dict = db[username]
-        return UserIn_Pydantic(**user_dict)
-    return None
+async def get_user(username: str) -> User | None:
+    try:
+        user = await User.get(email=username, is_active=True)
+        return user
+    except Exception as err:
+        raise HTTPException(status_code=400, detail="Inactive user") from err
 
 
-def authenticate_user(fake_db, username: str, password: str) -> User | Literal[False]:
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not user.verify_password(password):
-        return False
-    return user
+async def authenticate_user(username: str, password: str) -> User:
+    user_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Invalid User"
+    )
+    try:
+        user = await get_user(username)
+        if not user or not user.verify_password(password):
+            raise user_exception
+        return user
+    except Exception as err:
+        raise user_exception from err
 
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
@@ -99,28 +94,23 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme_cookie)]):
-    logger.info(token)
-    credentials_exceptions = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Cloud not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+    bad_request_exception = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="your request is not valid"
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str | None = payload.get("sub")
         if username is None:
-            raise credentials_exceptions
+            raise bad_request_exception
     except JWTError as err:
-        raise credentials_exceptions from err
-    user = get_user(fake_users_db, username=username)
+        raise bad_request_exception from err
+    user = await get_user(username=username)
     if user is None:
-        raise credentials_exceptions
+        raise bad_request_exception
     return user
 
 
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
