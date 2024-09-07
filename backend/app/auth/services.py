8749dffeaf89314e8a5148dd2aaada89ef1bfb28
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-09-07 12:26:28 +0900

    fix: パスワードのハッシュロジックを修正

--- backend/app/auth/services.py
+++ backend/app/auth/services.py
@@ -1,7 +1,5 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from passlib.hash import bcrypt
 
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    return bcrypt.hash(password)
