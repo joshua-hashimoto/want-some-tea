1018968b32c4b52053d93fd3e3e960f7c6c506be
Hashimoto Joshua <coffeesteam.jh38+git@protonmail.com>
2024-09-07 12:26:16 +0900

    fix: 不要な認証Schemaを削除

--- backend/app/auth/schemas.py
+++ backend/app/auth/schemas.py
@@ -1,19 +1,8 @@
-from typing import Union
-
 from pydantic import BaseModel
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Union[str, None] = None
 
 
 class SignUpRequest(BaseModel):
     email: str
-    nichname: str
+    nickname: str
     password: str
     confirm_password: str
